5f665406104173a9084ad5b7cff9fafe73203f5a
physworld <vladtkalenko@gmail.com>
2020-11-24 13:50:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [messages, setMessages] = useState([])
+  const [currentPage, setCurrentPage ] = useState(1)
+  const [fetching, setFetching] = useState(true)
+
+
+
+useEffect(( ) => {
+  if(fetching) {
+    axios.get(`https://jsonplaceholder.typicode.com/comments?_limit=23&_page=${currentPage}`)
+    .then(response => 
+      setMessages([...messages, ...response.data]),
+      setCurrentPage( prevState => prevState + 1))
+
+      .finally( () => setFetching (false));
+  }  
+},[fetching])
+
+useEffect(() => {
+  document.addEventListener('scroll', scrollHandler)
+  return function()
+  {
+    document.removeEventListener('scroll', scrollHandler)
+  };
+}, [])
+
+  const scrollHandler = (e) => {
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 ){
+      setFetching(true);
+    }
+
+  }
+
+  return(
+    <div className = {'app'}>
+      {messages.map(message =>
+        <div key={message.id}>
+            <div className = "message"> {message.id}. {message.body}</div>
+        </div>
+        )}
     </div>
   );
 }
